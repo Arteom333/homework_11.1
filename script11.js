9b2fca3a90e321108e3fee42c623c46eb8865ee2
Arteom333 <porchiesku@bk.ru>
2018-11-11 18:46:57 +0200

    APT

---
+++ script11.js
@@ -0,0 +1,22 @@
+var yourBrowser = "Chrome";
+var ieLog = "We don't support IE browser!";
+var webKitLog = "We support your browser = ";
+var otherLog = "We hope your specific browser works fine";
+
+var switchResult;
+switch (yourBrowser) {
+
+    case "IE":
+        switchResult = ieLog;
+        break;
+
+    case "Chrome" || "Firefox" || "Opera" || "Safari":
+        switchResult = webKitLog + yourBrowser;
+        break;
+
+    default:
+        switchResult = otherLog;
+
+}
+
+console.log('SWITCH SOLUTION: ', switchResult);
